63f5d2d4f55cde735187139e55a41602a10ef78c
Zeff01 <jzeffsomera@gmail.com>
2023-07-18 20:44:44 +0800

    text-black

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -3,7 +3,7 @@ import Card from "../components/BusinessCard";
 
 const Index = () => {
   return (
-    <div className="flex  justify-center py-2 bg-gray-200">
+    <div className="flex  justify-center py-2 bg-gray-200 text-black">
       <Card />
     </div>
   );
